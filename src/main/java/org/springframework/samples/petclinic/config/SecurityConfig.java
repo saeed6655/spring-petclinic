a1d0b4a203bd59df5f0caac8491e7b4ae213b1ca
saeed6655 <mo098hadi@gmail.com>
2025-07-09 15:20:48 +0300

    Create SecurityConfig.java
    Signed-off-by: saeed6655 <mo098hadi@gmail.com>

---
+++ src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/", "/welcome").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults())
+            .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
